1f707fa2db675553eb4f66361719e212540a4ab5
LankaHsu <lankahsu@gmail.com>
2022-09-18 08:23:36 +0800

    update mqtt

--- mqtt/mqtt_pub.sh
+++ mqtt/mqtt_pub.sh
@@ -6,25 +6,25 @@ PWD=`pwd`
 
 DAEMON="mosquitto_pub"
 
-PJ_MQTT_TOPIC_WANT=$1
-PJ_MQTT_BODY=$2
+MQTT_TOPIC_WANT=$1
+MQTT_BODY=$2
 
-PJ_MQTT_IP=127.0.0.1
-PJ_MQTT_PORT=1883
-PJ_MQTT_USER=apple
-PJ_MQTT_PASS=apple520
-PJ_MQTT_USER_PASS="-u $PJ_MQTT_USER -P $PJ_MQTT_PASS"
-PJ_MQTT_CERT=""
-#PJ_MQTT_CERT="--cafile /tmp/mqtt.ca --cert /tmp/mqtt_beex.crt --key /tmp/mqtt_beex.key"
+MQTT_IP=127.0.0.1
+MQTT_PORT=1883
+MQTT_USER=apple
+MQTT_PASS=apple520
+MQTT_USER_PASS="-u $MQTT_USER -P $MQTT_PASS"
+MQTT_CERT=""
+#MQTT_CERT="--cafile /tmp/mqtt.ca --cert /tmp/mqtt_beex.crt --key /tmp/mqtt_beex.key"
 
-PJ_MQTT_TOPIC_PREFIX="$PJ_MQTT_USER"
-PJ_MQTT_TOPIC="$PJ_MQTT_TOPIC_PREFIX/$PJ_MQTT_TOPIC_WANT"
-[ "$PJ_MQTT_TOPIC" != "" ] && PJ_MQTT_TOPIC_ARG="-t '$PJ_MQTT_TOPIC'"
+MQTT_TOPIC_PREFIX="$MQTT_USER"
+MQTT_TOPIC="$MQTT_TOPIC_PREFIX/$MQTT_TOPIC_WANT"
+[ "$MQTT_TOPIC" != "" ] && MQTT_TOPIC_ARG="-t '$MQTT_TOPIC'"
 
-PJ_MQTT_BODY_ARG="-m '{}'"
-[ "$PJ_MQTT_BODY" != "" ] && PJ_MQTT_BODY_ARG="-m '$PJ_MQTT_BODY'"
+MQTT_BODY_ARG="-m '{}'"
+[ "$MQTT_BODY" != "" ] && MQTT_BODY_ARG="-m '$MQTT_BODY'"
 
-PJ_MQTT_ARG="-h $PJ_MQTT_IP -p $PJ_MQTT_PORT $PJ_MQTT_USER_PASS $PJ_MQTT_CER $PJ_MQTT_TOPIC_ARG $PJ_MQTT_BODY_ARG"
+MQTT_ARG="-h $MQTT_IP -p $MQTT_PORT $MQTT_USER_PASS $MQTT_CER $MQTT_TOPIC_ARG $MQTT_BODY_ARG"
 
 die_fn()
 {
@@ -36,7 +36,7 @@ start_fn()
 {
 	#** COMMAND **
 	DO_COMMAND_EXE="$DAEMON"
-	DO_COMMAND="$DO_COMMAND_EXE $PJ_MQTT_ARG"
+	DO_COMMAND="$DO_COMMAND_EXE $MQTT_ARG"
 
 	echo "[$DO_COMMAND]"
 	sh -c "$DO_COMMAND"
@@ -51,7 +51,7 @@ showusage_fn()
 	return 0
 }
 
-[ "$PJ_MQTT_TOPIC_WANT" != "" ] || showusage_fn
+[ "$MQTT_TOPIC_WANT" != "" ] || showusage_fn
 
 main_fn()
 {
